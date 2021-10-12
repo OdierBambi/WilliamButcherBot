82b2a637b227c0c4da72ac551c4c17f22ab9cc6c
OdierBambi <88575320+OdierBambi@users.noreply.github.com>
2021-10-12 18:54:53 +0700

    Update misc.py

--- wbb/modules/misc.py
+++ wbb/modules/misc.py
@@ -194,25 +194,31 @@ async def random(_, message):
 # Translate
 @app.on_message(filters.command("tr") & ~filters.edited)
 @capture_err
-async def tr(_, message):
-    if len(message.command) != 2:
-        return await message.reply_text("/tr [LANGUAGE_CODE]")
-    lang = message.text.split(None, 1)[1]
-    if not message.reply_to_message or not lang:
-        return await message.reply_text(
-            "Reply to a message with /tr [language code]"
-            + "\nGet supported language list from here -"
-            + " https://py-googletrans.readthedocs.io/en"
-            + "/latest/#googletrans-languages"
-        )
-    reply = message.reply_to_message
-    text = reply.text or reply.caption
-    if not text:
-        return await message.reply_text("Reply to a text to translate it")
-    result = await arq.translate(text, lang)
-    if not result.ok:
-        return await message.reply_text(result.result)
-    await message.reply_text(result.result.translatedText)
+async def tr(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tl")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
+				await message.reply_text(f"Translated from **{fromt.capitalize()}** To **{to.capitalize()}**\n\n```{translation.text}```")
+			except:
+			   	await message.reply_text(f"Translated from **{translation.src}** To **{translation.dest}**\n\n```{translation.text}```")
+      			
+				
+			
+		except :
+			print("error")
+	else:
+			 ms = await message.reply_text("You can Use This Command by using reply to message")
+			 await ms.delete()
 
 
 @app.on_message(filters.command("json") & ~filters.edited)
